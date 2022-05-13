4947aa31b1fb03d995b8c1fcbd4d8eb89554e3bc
jose-abel <guest1@guest1s-MacBook-Pro.local>
2022-05-13 17:39:26 -0400

    Fixing linters errors

--- config/initializers/rswag-ui.rb
+++ config/initializers/rswag-ui.rb
@@ -1,5 +1,6 @@
-Rswag::Ui.configure do |c|
+# rubocop: disable Naming/FileName
 
+Rswag::Ui.configure do |c|
   # List the Swagger endpoints that you want to be documented through the swagger-ui
   # The first parameter is the path (absolute or relative to the UI host) to the corresponding
   # endpoint and the second is a title that will be displayed in the document selector
@@ -12,3 +13,5 @@ Rswag::Ui.configure do |c|
   # c.basic_auth_enabled = true
   # c.basic_auth_credentials 'username', 'password'
 end
+
+# rubocop: enable Naming/FileName
--- spec/requests/api/users/posts_controller_request_spec.rb
+++ spec/requests/api/users/posts_controller_request_spec.rb
@@ -1,25 +1,24 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/users/posts', type: :request do
-
   path '/api/users/{user_id}/posts' do
     get('list posts') do
       produces 'application/json'
       parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
       parameter({
-        :in => :header,
-        :type => :string,
-        :name => :Authorization,
-        :required => true,
-        :description => 'Client token'
-      })
+                  in: :header,
+                  type: :string,
+                  name: :Authorization,
+                  required: true,
+                  description: 'Client token'
+                })
 
       let(:user) { create(:user) }
       let(:user_id) { user.id }
       let!(:articles) { create_list(:article, 10, user: user) }
       let!(:Authorization) { token_generator(user.id) }
 
-      response(200, 'successful') do     
+      response(200, 'successful') do
         run_test! do |response|
           json_response = JSON.parse(response.body)
 
